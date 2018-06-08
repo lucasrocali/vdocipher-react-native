6ba621f2fa61c6299b620906ea77e8c0b2909212
Vikas <vkq0011@gmail.com>
2018-06-08 17:09:44 +0530

    avoid NPE

--- android/src/main/java/com/vdocipher/rnbridge/VdoPlayerControlView.java
+++ android/src/main/java/com/vdocipher/rnbridge/VdoPlayerControlView.java
@@ -239,7 +239,7 @@ public class VdoPlayerControlView extends FrameLayout {
             return;
         }
 
-        int playbackState = player.getPlaybackState();
+        int playbackState = player != null ? player.getPlaybackState() : -1;
         boolean playing = player != null
                 && playbackState != VdoPlayer.STATE_IDLE && playbackState != VdoPlayer.STATE_ENDED
                 && player.getPlayWhenReady();
