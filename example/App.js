3f1cf2ed8f88685a0b8c0fbd0a6145b2adc8124e
Vikas <vkq0011@gmail.com>
2019-11-14 16:45:58 +0530

    upgrade to rn 0.58.0

--- example/App.js
+++ example/App.js
@@ -1,7 +1,10 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
+ *
+ * @format
  * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
 import React, { Component } from 'react';
--- example/index.js
+++ example/index.js
@@ -1,3 +1,8 @@
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 
