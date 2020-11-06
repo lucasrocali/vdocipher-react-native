7430a73ad2c2bd69b52bde90ebbcf6ec9a7f7040
Vikas <vkq0011@gmail.com>
2020-11-06 10:09:06 +0530

    ios build fix

---
+++ index.ios.js
@@ -0,0 +1,24 @@
+/**
+ * The native VdoCipher player module exposes a function 'startVideoScreen'
+ * which takes the following parameters:
+ *
+ * 1. String params = json_stringify({
+ *                                     embedInfo: {
+ *                                        otp: string,
+ *                                        playbackInfo: string
+ *                                     }
+ *                                  })
+ */
+import { NativeModules } from 'react-native';
+import VdoPlayerView from './VdoPlayerView';
+
+const { VdocipherRnBridge } = NativeModules;
+
+const startVideoScreen = (params) => {
+  VdocipherRnBridge.startVideoScreen(params);
+}
+
+export {
+  startVideoScreen,
+  VdoPlayerView
+}
