a0c00c80e2bb668f76e3b01c0094f7d99e8f51db
Vikas <vkq0011@gmail.com>
2021-09-06 13:05:13 +0530

    update dependency react-native-gesture-handler

--- example/App.js
+++ example/App.js
@@ -14,7 +14,6 @@ import {
   Button,
   View
 } from 'react-native';
-import 'react-native-gesture-handler';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { startVideoScreen } from 'vdocipher-rn-bridge';
